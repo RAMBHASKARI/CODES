b37cdce94e532a7880f2bba05fd47b26d86c2e54
RAMBHASKARI <115989722+RAMBHASKARI@users.noreply.github.com>
2022-11-29 20:26:20 +0530

    Delete 3q.py

--- 3q.py
+++
@@ -1,6 +0,0 @@
-s1=input("Enter first String : ");
-s2=input("Enter second String : ");
-print("First string is : ",s1);
-print("Second string is : ",s2);
-print("concatenations of two strings :",s1+s2);
-print("Substring of given string :",s1[1:4]);
